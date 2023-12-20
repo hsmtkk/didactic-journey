91743853be9b403374eaeea628fef4a59f9f92c2
hsmtkk <hsmtkk@gmail.com>
2023-12-20 17:37:29 +0900

    add SideMenu component

---
+++ components/SideMenu.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app>
+        <v-navigation-drawer v-model="drawer">
+            <v-sheet color="grey-lighten-4" class="pa-4">
+                <v-avatar class="mb-4" color="grey-darken-1" size="64"></v-avatar>
+
+                <div>john@google.com</div>
+            </v-sheet>
+
+            <v-divider></v-divider>
+
+            <v-list>
+                <v-list-item v-for="[icon, text] in links" :key="icon" :prepend-icon="icon" :title="text"
+                    link></v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </v-app>
+</template>
+
+<script setup lang="ts">
+const drawer = ref(null)
+const links = [
+    ['mdi-inbox-arrow-down', 'Inbox'],
+    ['mdi-send', 'Send'],
+    ['mdi-delete', 'Trash'],
+    ['mdi-alert-octagon', 'Spam'],
+]
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+    <SideMenu />
+    <p>Index</p>
+</template>
